1346f50f0fba23e7e27f9690c5e70721c994d9da
ryo-344 <knoooot1ump@yahoo.co.jp>
2020-08-11 21:43:52 +0900

    バリデーションの設定 & エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
+  validates :lastnamekana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: "Full-width katakana characters" } 
+  validates :firstnamekana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: "Full-width katakana characters" }
+  validates :birthday, presence: true
 end
