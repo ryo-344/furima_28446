aedc2d3073ac1f2ebaeac89247b4bd48d3c112ef
ryo-344 <knoooot1ump@yahoo.co.jp>
2020-08-13 16:29:44 +0900

    createアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,17 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category, :status, :shippingorigin, :burden, :days, :price)
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
