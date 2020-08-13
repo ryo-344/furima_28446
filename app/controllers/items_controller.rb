3a83ec72e28fec81a34437ee0c7733dd36f6197c
ryo-344 <knoooot1ump@yahoo.co.jp>
2020-08-13 12:08:15 +0900

    商品出品画面への遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,10 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index]
 
   def index
+  end
+
+  def new
   end
 
   def move_to_index
