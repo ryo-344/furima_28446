44166607d2b541f640433838e77351364ed7bc4b
ryo-344 <knoooot1ump@yahoo.co.jp>
2020-08-13 14:26:40 +0900

    モデルのアソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :items
   validates :nickname, presence: true
   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
   validates :lastname, presence: true, format: { with: NAME_REGEX, message: 'Full-width characters' }
