7a54404c9f026496f2befb07a06cb784777b6cf1
ryo-344 <knoooot1ump@yahoo.co.jp>
2020-08-14 12:34:15 +0900

    バリデーション の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,14 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shippingorigin
   belongs_to_active_hash :burden
   belongs_to_active_hash :days
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true , inclusion: {in: 2..11}
+  validates :status_id, presence: true, inclusion: {in: 2..7}
+  validates :shippingorigin_id, presence: true, inclusion: {in: 2..48}
+  validates :burden_id, presence: true, inclusion: {in: 2..3}
+  validates :days_id, presence: true, inclusion: {in: 2..4}
+  validates :price, presence: true, inclusion: {in: 300..9999999}
 
 end
