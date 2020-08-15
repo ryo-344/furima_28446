e332ca095e4cfe74bab633799f5c56c367400cc1
ryo-344 <knoooot1ump@yahoo.co.jp>
2020-08-15 21:40:15 +0900

    テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Team.name}
+    explanation {Faker::Lorem.sentence}
+    category_id {2}
+    status_id {2}
+    shippingorigin_id {2}
+    burden_id {2}
+    days_id {2}
+    price {500}
+    association :user
   end
 end
