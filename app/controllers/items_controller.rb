90124fcc4bc2d36ba58d79f7eb3a8408d2e73fa5
ryo-344 <knoooot1ump@yahoo.co.jp>
2020-08-17 21:39:38 +0900

    編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only:[:show, :edit]
+  before_action :set_item, only:[:show, :edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -25,7 +25,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
