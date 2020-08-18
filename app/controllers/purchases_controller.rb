b5afda34999e261c1788c80dd7d664efaac5eef2
ryo-344 <knoooot1ump@yahoo.co.jp>
2020-08-18 19:22:28 +0900

    購入コントローラーの編集とビューの編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,24 @@
 class PurchasesController < ApplicationController
+before_action :move_to_sign_in
+before_action :move_to_top
 
   def index
+    @item = Item.find_by(id: params[:format])
   end
 
   def create
   end
-  
+
+  private
+
+  def move_to_sign_in
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  def move_to_top
+    @item = Item.find_by(id: params[:format])
+    redirect_to root_path if current_user.id == @item.user_id
+    redirect_to root_path if @item.purchase.present?
+  end
+
 end
