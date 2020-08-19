ac397f9577f9e282234b03d42938f3782e8a9c22
ryo-344 <knoooot1ump@yahoo.co.jp>
2020-08-19 17:33:00 +0900

    クレジット決済機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,19 @@
 class PurchasesController < ApplicationController
 before_action :move_to_sign_in
-before_action :move_to_top
-
+before_action :move_to_top, except: [:create]
   def index
-    @item = Item.find_by(id: params[:format])
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @order = Purchase.new(user_id: order_params[:user_id], item_id: order_params[:item_id])
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else  
+      render 'index'
+    end
   end
 
   private
@@ -16,9 +23,23 @@ before_action :move_to_top
   end
 
   def move_to_top
-    @item = Item.find_by(id: params[:format])
-    redirect_to root_path if current_user.id == @item.user_id
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == params[:item_id]
     redirect_to root_path if @item.purchase.present?
+  end
+
+  def order_params
+    params.permit(:token).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
